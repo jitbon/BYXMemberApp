d57868c78ba8bafde8bfdea634242d86f5f5ba20
Blaine Mitchell <blaine.z.mitchell@vanderbilt.edu>
2021-10-10 17:23:44 -0500

    messing around with mysql

--- login.py
+++ login.py
@@ -1,4 +1,6 @@
 from main import app
+import flask.scaffold
+flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
 from flask_restful import Api, Resource
 
 api = Api(app)
@@ -7,3 +9,13 @@ class Login(Resource):
     pass
 
 api.add_resource(Login, '/login')
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="admin",
+    password="admin"
+)
+
+print(mydb)
