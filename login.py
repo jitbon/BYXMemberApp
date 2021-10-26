a0b77d039dd55cce95f7da5d6528fb3d0fff7e03
Blaine Mitchell <blaine.z.mitchell@vanderbilt.edu>
2021-10-26 17:47:47 -0500

    backend updated for member database feature

--- login.py
+++ login.py
@@ -1,3 +1,4 @@
+import re
 from main import app
 from flask import request
 import mysql.connector
@@ -7,7 +8,7 @@ from flask_restful import Api
 
 api = Api(app)
 
-@app.route("/login", methods=['GET'])
+@app.route("api/login/", methods=['GET'])
 def login():
     username = request.args.get("username")
     password = request.args.get("password")
@@ -16,6 +17,26 @@ def login():
     if True:
         return "Logged in", 200
     return "Incorrect username or password", 401
+
+@app.route("api/members/", methods=['GET'])
+def get_members():
+    # TODO: poll database for all members and return them
+    return "", 200
+
+@app.route("api/members/", methods=['POST'])
+def add_new_member():
+    # TODO: add member to database
+    return "", 200
+
+@app.route("api/members/", methods=['PUT'])
+def update_member():
+    #TODO: update member in database
+    return "", 200
+
+@app.route("api/members", methods=['DELETE'])
+def delete_member():
+    # TODO: delete member from database
+    return "", 200
 
 mydb = mysql.connector.connect(
     host="localhost",
