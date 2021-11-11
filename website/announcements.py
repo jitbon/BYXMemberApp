07e2f9ee50077aba2d360fb70bb35c94ef6879aa
Paul <pauljhwoo@gmail.com>
2021-11-11 10:06:34 -0600

    revamp

--- website/announcements.py
+++
@@ -1,8 +0,0 @@
-# Group 5
-# Jason Kim (jason.j.kim@vanderbilt.edu)
-# Blaine Mitchell (blaine.z.mitchell@vanderbilt.edu)
-# Bo Peng (bo.peng@vanderbilt.edu)
-# Paul Woo (paul.woo@vanderbilt.edu)
-# Homework 3
-
-# The announcements file, handling announcements made by administrators and viewable by standard members
