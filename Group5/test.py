4fed6d43dc9260de7a9242ad62b98a95ef55b8f9
Paul <pauljhwoo@gmail.com>
2021-11-11 20:48:13 -0600

    new test

---
+++ Group5/test.py
@@ -0,0 +1,30 @@
+import os
+import tempfile
+from Group5.routes import render_template
+
+import pytest
+
+from Group5 import app
+
+
+@pytest.fixture
+def client():
+    return app.test_client()
+
+def test_home(client):
+    resp = client.get('/')
+    assert resp.status_code == 200
+
+def test_home_bad_http_method(client):
+    resp = client.post('/')
+    assert resp.status_code == 405
+
+def test_account(client):
+    resp = client.post('/account', data='something')
+    assert resp.status_code == 400
+    assert resp.
+
+def test_account(client):
+    resp = client.post('/account')
+    assert resp.status_code == 400
+
