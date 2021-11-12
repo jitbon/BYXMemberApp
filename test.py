53f6e38853150bd44d10d771488af912f19bcb38
Paul <pauljhwoo@gmail.com>
2021-11-11 20:58:36 -0600

    more tests

--- test.py
+++ test.py
@@ -23,7 +23,38 @@ def test_account(client):
     resp = client.post('/account', data='something')
     assert resp.status_code == 400
 
-def test_account(client):
+def test_account_bad_http_method(client):
     resp = client.post('/account')
     assert resp.status_code == 400
+    
+def test_search(client):
+    resp = client.post('/announcements', data='something')
+    assert resp.status_code == 400
 
+def test_search_bad_http_method(client):
+    resp = client.post('/announcements')
+    assert resp.status_code == 400
+    
+def test_register(client):
+    resp = client.post('/register', data='something')
+    assert resp.status_code == 400
+
+def test_register_bad_http_method(client):
+    resp = client.post('/register')
+    assert resp.status_code == 400
+
+def test_schedule(client):
+    resp = client.post('/schedule', data='something')
+    assert resp.status_code == 400
+
+def test_schedule_bad_http_method(client):
+    resp = client.post('/schedule')
+    assert resp.status_code == 400
+
+def test_insert(client):
+    resp = client.post('/insert', data='something')
+    assert resp.status_code == 400
+
+def test_insert_bad_http_method(client):
+    resp = client.post('/insert')
+    assert resp.status_code == 400
