0a3dd68956080d522ec8939947fa8f25bb5b0b72
BPeng21 <bo.peng@vanderbilt.edu>
2021-11-18 11:45:27 -0600

    re-added schedule function

--- Group5/routes.py
+++ Group5/routes.py
@@ -9,6 +9,7 @@ from Group5.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 from flask_mail import Message
 from datetime import datetime
+from quickstart import scheduleEvent
 
 @app.route("/")
 def intro():
